eda18858f0eaa5009a6c95c5077f9aa5506427c1
sashamichelle <sashaavilescalderon1999@gmail.com>
2017-10-05 11:00:31 -0300

    mi codigo de tarjeta valida

---
+++ app.js
@@ -0,0 +1,18 @@
+function isValidCard(answer,position){//(([2,4,6]),([1,2,3])){
+  var array= [];
+   var newArray=answer.reverse();
+  //retorna el arreglo al reverse
+    return newArray;// retornaria [6,4,2];
+    //creando posicion para comparar ambos digitos
+     var position =[];//[1,2,3]
+
+     for(var i =0;newArray.legth==position.length;i++){
+     //El indice de myNewArray tiene que ser el mismo al de position
+  //colocando condición--->revisar este error
+  if (newArray.indexOF(i) % 0 && position.indexOF(i) % 0){
+ var result= (newArray.indexOF(i) * position.indexOF(i));}
+  var multiply= result;
+}
+ }
+  return multiply[result];
+  }
